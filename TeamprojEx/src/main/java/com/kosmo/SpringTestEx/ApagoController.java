e7090e701cc1bb77fd10f13ec6c55d8ef0e89fb7
jungmin <49jungmin@gmail.com>
2018-08-29 15:00:54 +0900

    0829-3시커밋

--- TeamprojEx/src/main/java/com/kosmo/SpringTestEx/ApagoController.java
+++ TeamprojEx/src/main/java/com/kosmo/SpringTestEx/ApagoController.java
@@ -1,10 +1,21 @@
 package com.kosmo.SpringTestEx;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -66,4 +77,119 @@ public class ApagoController {
 		return "07Mybatis/list";
 	}
 	
+	@RequestMapping("/mybatis/login.do")
+	public String login(Model model, HttpSession session) {
+		return "member/login";
+	}
+	
+	@RequestMapping("/Captcha")
+	public String captcha(Model model, HttpSession session, HttpServletRequest req, HttpServletResponse resp) {
+		String clientId = "42zBWTFtjL22rQYfvhob";
+	      String clientSecret = "svMjUgPpxT";
+	      try {
+	          String code = "0"; // 키 발급 받을 때는 0으로 설정
+	          String apiURL = "https://openapi.naver.com/v1/captcha/nkey?code=" + code;
+	          URL url = new URL(apiURL);
+	          HttpURLConnection con = (HttpURLConnection)url.openConnection();
+	          con.setRequestMethod("GET");
+	          con.setRequestProperty("X-Naver-Client-Id", clientId);
+	          con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+	          int responseCode = con.getResponseCode();
+	          BufferedReader br;
+	          if(responseCode==200) { 
+	              br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	          } else {  
+	              br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	          }
+	          String inputLine;
+	          StringBuffer response = new StringBuffer();
+	          while ((inputLine = br.readLine()) != null) {
+	              response.append(inputLine);
+	          }
+	          br.close();
+	          System.out.println(response.toString());
+	          resp.setContentType("text/html;charset=UTF-8");
+	          
+				
+				//서블릿에서 세션객체를 얻어온다.
+				session = req.getSession();
+				
+				session.setAttribute("key", response.toString());
+	          
+	      } catch (Exception e) {
+	          System.out.println(e);
+	      }
+		
+		return "member/captcha";
+	}
+	
+	@RequestMapping("/CaptchaRes")
+	public String result(Model model, HttpSession session, HttpServletRequest req, HttpServletResponse resp) {
+		
+		String clientId = "42zBWTFtjL22rQYfvhob";
+        String clientSecret = "svMjUgPpxT";
+        session = req.getSession();
+        StringBuffer response = new StringBuffer();
+        String apiResult = null;
+        try {
+            String code = "1"; 
+            String key = session.getAttribute("vkey").toString(); 
+            String value = req.getParameter("inputText"); 
+            String apiURL = "https://openapi.naver.com/v1/captcha/nkey?code=" + code +"&key="+ key + "&value="+ value;
+
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { 
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            //response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        apiResult = response.toString();
+        System.out.println(apiResult);
+        
+		JSONParser parser = new JSONParser();
+		Object obj = null;
+		try {
+			obj = parser.parse(apiResult);
+		} catch (ParseException e) {
+			
+			e.printStackTrace();
+		}
+		JSONObject jsonobj = (JSONObject) obj;
+
+		Boolean result = (Boolean) jsonobj.get("result");
+        if(result == true) {
+        	//한글처리
+			resp.setContentType("text/html;charset=UTF-8");
+			
+			return "member/join_agree";
+        }else {
+        	//한글처리
+			resp.setContentType("text/html;charset=UTF-8");
+			//서블릿에서 out객체를 얻어온다.
+			return "member/false";
+        }		
+		
+	}
+	
+	@RequestMapping("/mybatis/join.do")
+	public String join(Model model, HttpSession session) {
+		return "member/join";
+	}	
 }
--- TeamprojEx/src/main/webapp/WEB-INF/views/home.jsp
+++ TeamprojEx/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,36 +1,117 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<html>
+   pageEncoding="UTF-8"%>
+
+
+<!DOCTYPE html>
+ 
 <head>
-	<title>Home</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta http-equiv="Pragma" CONTENT="no-cache">
+<meta http-equiv="Expires" CONTENT="-1">
+
+<meta name="Description" content="아파고병원">
+<title>인제대학교 서울백병원</title>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="<%=request.getContextPath() %>/resources/css/base.css">
+<link rel="stylesheet" href="<%=request.getContextPath() %>/resources/css/ui_style.css?180711">
+<link rel="stylesheet" href="<%=request.getContextPath() %>/resources/css/common.css?180604">
+<link rel="stylesheet" href="<%=request.getContextPath() %>/resources/css/layout.css?180315">
+<link rel="stylesheet" href="<%=request.getContextPath() %>/resources/css/content.css?1807172">
+<link rel="stylesheet" href="<%=request.getContextPath() %>/resources/css/colorChange.css">
+<link rel="stylesheet" href="<%=request.getContextPath() %>/resources/css/content_seoul.css?1805141">
+<%-- <script type="text/javascript" src="<%=request.getContextPath() %>/js/libs/jquery-1.11.2.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/js/libs/jquery-ui-1.12.1.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/js/libs/jquery.browser.check.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/js/libs/modernizr.min.js?180413"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/js/plugins.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/js/ui.js?1807051"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="/common/js/libs/html5shiv-printshiv.js"></script>
+<![endif]-->
+
+<script>
+   $(function(){
+
+      if (getCookie('Mo')!='false') {
+         if(isMobile()){
+            if(confirm("모바일기기로 접근하셨습니다. 모바일페이지로 이동하시겠습니까?    ")){
+               location.href = "/m/seoul/";
+            }else{
+               setCookie('Mo', 'false', 1);
+            }
+         }
+      }
+
+      $("[data-img]").each(function(){
+         var img =  $(this).attr("data-img");
+         $(this).css("background-image","url("+img+")");
+      });
+      var infiniteLoop = null
+         , pager = null
+         , liCount = $('.img-area').find('li').length;
+
+      if (liCount <=3) {
+         infiniteLoop = false;
+         pager = false;
+      } else {
+         infiniteLoop = true;
+         pager = true;
+      }
+
+      $('.img-area .bxslider').bxSlider({
+         slideWidth: 356,
+         slideMargin: 6,
+         controls: false,
+         stopAutoOnClick: false,
+         touchEnabled: false,
+         moveSlides:1,
+         minSlides:0,
+         maxSlides:3,
+         pager: pager,
+         infiniteLoop: infiniteLoop
+      });
+
+      $(".news-ticker .bxslider").bxSlider({
+         mode: 'vertical',
+         auto: true,
+         speed: 400,
+         autoControls: false,
+         stopAutoOnClick: false,
+         controls: false,
+         touchEnabled: false,
+         pager: false
+      });
+
+      // bxsliderThumb('.img-area .bxslider',360,0,0,3,1);
+      bxsliderThumb('.banner-slider .bxslider',267,4,0,4,1);
+      bxslider('.press-box .bxslider');
+
+   });
+</script> --%>
 </head>
 <body>
+<!-- 상단 영역 -->
+<%@ include file="./include/sourcecopy_header.jsp" %>
 <div class="container">
-	<h2>스프링 MVC 시작하기</h2>
+
 	
-	<h2>First 컨트롤러 만들기</h2>
-	<li>
-		<a href="./home/helloSpring" target="_blank">
-			First 컨트롤러
-		</a>
-	</li>
+	<!-- 컨텐츠 영역 -->
+	<div class="row">
+		프론트페이지 컨텐츠 내용입니다
+		<br />
+		<img src="../images/inner1.jpg" width="80%" height="600px;" />
+	<br /><br /><br /><br /><br />
+	<br /><br /><br /><br /><br />
+	<br /><br /><br /><br /><br />
+	<br /><br /><br /><br /><br />
+	<br /><br /><br /><br /><br />
 	
-	<h2>웹채팅</h2>
-	<li>
-		<a href="./home/chat" target="_blank">
-			웹채팅
-		</a>
-	</li>
-	
-	<h3>아파GO</h3>
-	<li>
-		<a href="mybatis/list.do">
-			프론트페이지
-		</a>
-	</li>
-	
+	</div>
+ 
 </div>
+<%@ include file="./include/sourcecopy_bottom.jsp" %>
+
 </body>
 </html>
